3e9f07318b3177bdc6bc96368c9792cb8063bb67
madhumita08 <63968160+madhumita08@users.noreply.github.com>
2021-08-01 11:57:41 +0530

    Intial commit

---
+++ webapp.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import requests
+import json
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == "POST":
+        city = request.form['city']
+        country = request.form['country']
+        api_key = "170f6c6c1678c13b9a1e6a1abc9c86a2"
+
+        weather_url = requests.get(
+            f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=imperial')
+
+        weather_data = weather_url.json()
+        if(weather_data['cod'] == "404"):
+            return render_template("error.html",msg = "CITY NOT FOUND")
+
+        temp = round(weather_data['main']['temp'])
+        humidity = weather_data['main']['humidity']
+        wind_speed = weather_data['wind']['speed']
+
+        return render_template("result.html", temp=temp, humidity=humidity, wind_speed=wind_speed, city=city)
+
+
+    return render_template("index.html")
+if __name__ == '__main__':
+	app.run(debug= True)
